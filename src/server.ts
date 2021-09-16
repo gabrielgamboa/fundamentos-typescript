01459b4d2e00170ebe9b2e9b420f62010a16fdb3
gabrielgamboa <gabriel_gamboa10@hotmail.com>
2021-09-16 17:21:01 -0300

    implementando conceitos SOLID na api

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,10 @@
 import express from "express";
+import { categoriesRoutes } from "./routes/categories.routes";
 
 const app = express();
 
 app.use(express.json());
 
-app.post("/", (request, response) => {
-    const {name} = request.body;
-    return response.json(name);
-});
+app.use("/categories", categoriesRoutes);
 
 app.listen(3333, () => console.log("Server is running on port 3333"));
