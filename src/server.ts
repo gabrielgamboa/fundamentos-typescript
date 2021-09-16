b9de67e68b6758b4da0a918dfec5a80c481289f1
gabrielgamboa <gabriel_gamboa10@hotmail.com>
2021-09-16 13:48:43 -0300

    adding launch.json

--- src/server.ts
+++ src/server.ts
@@ -2,4 +2,11 @@ import express from "express";
 
 const app = express();
 
+app.use(express.json());
+
+app.post("/", (request, response) => {
+    const {name} = request.body;
+    return response.json(name);
+});
+
 app.listen(3333, () => console.log("Server is running on port 3333"));
