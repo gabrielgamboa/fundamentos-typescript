071422703136e7928449f3903ad7026a4e73baac
gabrielgamboa <gabriel_gamboa10@hotmail.com>
2021-09-19 10:18:03 -0300

    colocando as implementações dos repositórios na pasta implementations

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,11 +1,9 @@
 import { Router } from "express";
 
-import { CategoriesRepository } from "../modules/cars/repositories/CategoriesRepository";
 import { createCategoryController } from "../modules/cars/useCases/createCategory/index"
 import { listCategoryController } from "../modules/cars/useCases/listCategories/index"; 
 
 const categoriesRoutes = Router();
-const categoriesRepository = new CategoriesRepository();
 
 //create a new category
 categoriesRoutes.post("/", (request, response) => {
--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -1,17 +1,15 @@
 import { Router } from "express";
-import { SpecificationsRepository } from "../modules/cars/repositories/SpecificationsRepository";
 import { CreateSpecificationService } from "../modules/cars/services/CreateSpecificationService";
 
 const specificationRoutes = Router();
-const specificationsRepository = new SpecificationsRepository();
 
 specificationRoutes.post("/", (request, response) => {
-    const { name, description } = request.body;
-    const createSpecificationService = new CreateSpecificationService(specificationsRepository);
+    // const { name, description } = request.body;
+    // const createSpecificationService = new CreateSpecificationService(specificationsRepository);
 
-    createSpecificationService.execute({ name, description });
+    // createSpecificationService.execute({ name, description });
 
-    return response.status(201).send();
+    // return response.status(201).send();
 });
 
 export { specificationRoutes }
