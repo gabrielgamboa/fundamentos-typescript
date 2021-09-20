b3d9dd79573e9e942bf0c8d634231efab0c9a548
gabrielgamboa <gabriel_gamboa10@hotmail.com>
2021-09-20 16:47:11 -0300

    adicionando multer no projeto

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,9 +1,14 @@
 import { Router } from "express";
+import multer from "multer";
 
 import { createCategoryController } from "../modules/cars/useCases/createCategory/index"
 import { listCategoryController } from "../modules/cars/useCases/listCategories/index"; 
 
 const categoriesRoutes = Router();
+
+const upload = multer({
+    dest: "./tmp",
+});
 
 //create a new category
 categoriesRoutes.post("/", (request, response) => {
@@ -13,6 +18,12 @@ categoriesRoutes.post("/", (request, response) => {
 //list all categories
 categoriesRoutes.get("/", (request, response) => {
     return listCategoryController.handle(request, response);
+});
+
+categoriesRoutes.post("/import", upload.single("file"), (request, response) => {
+    const { file } = request;
+    console.log(file);
+    return response.send();
 });
 
 export { categoriesRoutes };
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,12 @@
 import express from "express";
-import { categoriesRoutes } from "./routes/categories.routes";
-import { specificationRoutes } from "./routes/specifications.routes";
+
+import { router } from "./routes/index"
 
 const app = express();
 
 app.use(express.json());
+app.use(router);
 
-app.use("/categories", categoriesRoutes);
-app.use("/specification", specificationRoutes);
+
 
 app.listen(3333, () => console.log("Server is running on port 3333"));
