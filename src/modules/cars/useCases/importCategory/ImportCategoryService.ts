ebbec4491537adffbaa8e126592c3270ec1aaede
gabrielgamboa <gabriel_gamboa10@hotmail.com>
2021-09-22 17:43:17 -0300

    finalizando implementação da importação de categorias via arquivo csv

--- src/modules/cars/useCases/importCategory/ImportCategoryService.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryService.ts
@@ -1,19 +1,54 @@
 import fs from "fs";
 import csvParse from "csv-parse";
+import { ICategoriesRepository } from "../../repositories/ICategoriesRepository";
 
-class ImportCategoryService {
-    execute(file: Express.Multer.File): void {
-        const stream = fs.createReadStream(file.path);
-
-        const parseFile = csvParse();
-
-        stream.pipe(parseFile);
-
-        parseFile.on("data", async (line) => {
-            console.log(line);
-        });
-    }
+interface IImportCategory {
+    name: string;
+    description: string;
 }
 
+class ImportCategoryService {
+    private categoriesRepository: ICategoriesRepository;
+
+    constructor(categoriesRepository: ICategoriesRepository) {
+        this.categoriesRepository = categoriesRepository;
+    }
+
+    loadCategories(file: Express.Multer.File): Promise<IImportCategory[]> {
+        return new Promise((resolve, reject) => {
+            const stream = fs.createReadStream(file.path);
+            const categories: IImportCategory[] = [];
+
+            const parseFile = csvParse();
+
+            stream.pipe(parseFile);
+
+            parseFile.on("data", async (line) => {
+                const [name, description] = line;
+                categories.push({
+                    name,
+                    description
+                });
+            }).on("end", () => {
+                resolve(categories);
+            }).on("error", (err) => {
+                reject(err);
+            })
+        });
+    }
+
+    async execute(file: Express.Multer.File): Promise<void> {
+        const categories = await this.loadCategories(file);
+        categories.forEach(category => {
+            const { name, description } = category;
+
+            const categoryAlreadyExists = this.categoriesRepository.findByName(name);
+
+            if (!categoryAlreadyExists) {
+                this.categoriesRepository.create({name, description});
+            }
+        })
+    }
+}
 
 export { ImportCategoryService }
