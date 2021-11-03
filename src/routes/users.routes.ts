b25de5f3e21f31454e689d8846b945e6c4c1b335
gabrielgamboa <gabriel_gamboa10@hotmail.com>
2021-11-03 16:39:32 -0300

    finalizando rota para upload de avatar

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,12 +1,24 @@
-import { Router } from "express";
+import multer from "multer";
+import uploadConfig from "../config/upload";
+
 import { CreateUserController } from "../modules/accounts/useCases/createUser/CreateUserController";
+import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
+import { Router } from "express";
+import { UpdateUserAvatarController } from "../modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController";
 
 const usersRoutes = Router();
 
+const uploadAvatar = multer(uploadConfig.upload("./tmp/avatar"));
+
 const createUserController = new CreateUserController();
+const updateUserAvatarController = new UpdateUserAvatarController();
 
 usersRoutes.post("/", (request, response) => {
     return createUserController.handle(request, response);
 });
+
+usersRoutes.patch("/avatar", ensureAuthenticated, uploadAvatar.single("avatar"), (request, response) => {
+    return updateUserAvatarController.handle(request, response);
+})
 
 export { usersRoutes }
