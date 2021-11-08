70977457b3c21afd637985be2684a9b6baa655aa
gabrielgamboa <gabriel_gamboa10@hotmail.com>
2021-11-08 17:32:38 -0300

    criando função para deletar avatar que já estiver na pasta tmp

--- src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarService.ts
+++ src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarService.ts
@@ -1,4 +1,5 @@
 import { inject, injectable } from "tsyringe";
+import { deleteFile } from "../../../../utils/file";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
 
@@ -17,6 +18,11 @@ class UpdateUserAvatarService {
 
     async execute({user_id, avatar_file}: IRequest): Promise<void> {
         const user = await this.usersRepository.findById(user_id);
+
+        if(user.avatar) {
+            await deleteFile(`./tmp/avatar/${user.avatar}`);
+        }
+
         user.avatar = avatar_file;
 
         await this.usersRepository.create(user);
