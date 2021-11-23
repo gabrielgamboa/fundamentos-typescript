15bc34fc5e2a0c069078560af700c25aaad0fb07
gabrielgamboa <gabriel_gamboa10@hotmail.com>
2021-11-23 14:30:51 -0300

    isolando middleware de erro dentro da pasta middlewares

---
+++ src/middlewares/error.ts
@@ -0,0 +1,13 @@
+import { Request, Response, NextFunction } from "express";
+import { AppError } from "@errors/AppError";
+
+export const error = (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof AppError) {
+        return response.status(err.statusCode).json({ message: err.message });
+    }
+
+    return response.status(500).json({
+        status: "error",
+        message: `Internal server error - ${err.message} `
+    });
+}
