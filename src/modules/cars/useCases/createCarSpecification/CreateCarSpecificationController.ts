5aad01e2b69453e83d0bf84f00433ce26ac5e5e5
gabrielgamboa <gabriel_gamboa10@hotmail.com>
2022-01-26 10:58:27 -0300

    finalizando construção de inserir especificações no carro

---
+++ src/modules/cars/useCases/createCarSpecification/CreateCarSpecificationController.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from "express";
+import { container } from "tsyringe";
+import { CreateCarSpecificationService } from "./CreateCarSpecificationService";
+
+class CreateCarSpecificationController {
+    async handle(request: Request, response: Response): Promise<Response> {
+        const { id } = request.params;
+        const { specifications_id } = request.body;
+
+        const createCarSpecificationService = container.resolve(CreateCarSpecificationService);
+        const car = await createCarSpecificationService.execute({ car_id: id, specifications_id});
+
+        return response.json(car);
+    }
+}
+
+export { CreateCarSpecificationController }
--- src/shared/infra/http/routes/cars.routes.ts
+++ src/shared/infra/http/routes/cars.routes.ts
@@ -3,18 +3,26 @@ import { CreateCarController } from "@modules/cars/useCases/createCar/CreateCarC
 import { ListAvailableCarController } from "@modules/cars/useCases/listCars/ListAvailableCarsController";
 import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 import { ensureAdmin } from "../middlewares/ensureAdmin";
+import { CreateCarSpecificationController } from "@modules/cars/useCases/createCarSpecification/CreateCarSpecificationController";
 
 const carsRoutes = Router();
 
 const createCarController = new CreateCarController();
 const listAvailableCarsController = new ListAvailableCarController();
+const createCarSpecificationController = new CreateCarSpecificationController();
 
-carsRoutes.post("/", 
+carsRoutes.post("/",
     ensureAuthenticated,
     ensureAdmin,
     createCarController.handle
 );
 
 carsRoutes.get("/available", listAvailableCarsController.handle);
+
+carsRoutes.post("/specifications/:id", 
+    ensureAuthenticated,
+    ensureAdmin, 
+    createCarSpecificationController.handle
+);
 
 export { carsRoutes }
