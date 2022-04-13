999d3590e2f693b713cd9bd1be1ba5085d38a49f
gabrielgamboa <gabriel_gamboa10@hotmail.com>
2022-04-13 14:47:49 -0300

    finalização do upload de imagens

--- src/modules/cars/useCases/uploadCarImage/UploadCarImageController.ts
+++ src/modules/cars/useCases/uploadCarImage/UploadCarImageController.ts
@@ -1,5 +1,26 @@
-class UploadCarImageController {
+import { Request, Response } from "express";
+import { container } from "tsyringe";
+import { UploadCarImageService } from "./UploadCarImageService";
 
+
+interface IFiles {
+    filename: string;
+}
+class UploadCarImageController {
+    async handle(request: Request, response: Response): Promise<Response> {
+        const { id } = request.params;
+        const images = request.files as IFiles[];
+
+        console.log(images)
+
+        const uploadCarImage = container.resolve(UploadCarImageService);
+
+        const images_name = images.map(file => file.filename);
+
+        await uploadCarImage.execute({ car_id: id, images_name});
+
+        return response.status(201).send();
+    }
 }
 
 export { UploadCarImageController }
--- src/shared/infra/http/routes/cars.routes.ts
+++ src/shared/infra/http/routes/cars.routes.ts
@@ -4,12 +4,19 @@ import { ListAvailableCarController } from "@modules/cars/useCases/listCars/List
 import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 import { ensureAdmin } from "../middlewares/ensureAdmin";
 import { CreateCarSpecificationController } from "@modules/cars/useCases/createCarSpecification/CreateCarSpecificationController";
+import { UploadCarImageController } from "@modules/cars/useCases/uploadCarImage/UploadCarImageController";
+
+import multer from "multer";
+import uploadConfig from "../../../../config/upload";
 
 const carsRoutes = Router();
 
 const createCarController = new CreateCarController();
 const listAvailableCarsController = new ListAvailableCarController();
 const createCarSpecificationController = new CreateCarSpecificationController();
+const uploadCarImageController = new UploadCarImageController();
+
+const upload = multer(uploadConfig.upload("./tmp/avatar"));
 
 carsRoutes.post("/",
     ensureAuthenticated,
@@ -23,6 +30,13 @@ carsRoutes.post("/specifications/:id",
     ensureAuthenticated,
     ensureAdmin, 
     createCarSpecificationController.handle
+);
+
+carsRoutes.post("/images/:id",
+    ensureAuthenticated,
+    ensureAdmin,
+    upload.array("images"),
+    uploadCarImageController.handle
 );
 
 export { carsRoutes }
